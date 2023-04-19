fc8bea7a437c9dc440c0c57c4f75e4dc39d3081c
Serhii04 <fi.03.volinec.sergij@gmail.com>
2023-04-19 18:21:44 +0300

    Append initialising functions for AffineCryptographer

--- cp_3/volynets_fi-03_cp3/cp_proj_3.py
+++ cp_3/volynets_fi-03_cp3/cp_proj_3.py
@@ -5,8 +5,23 @@ from lab_constants import __RU_ALPHABET__ as alphabet
 
 
 class AffineCryptographer:
-    def __init__(self):
-        pass
+    def __init__(self, alphabet: str, leters_probability: list) -> None:
+        self.leters_probability = leters_probability.copy()
+        self.set_alphabet(alphabet=alphabet)
+
+    def set_alphabet(self, alphabet: str) -> None:
+        # self.most_frequent_leter = "о"
+
+        self.alphabet = alphabet
+        self.size = len(alphabet)
+
+        self.__id_to_letter = dict()
+        for id, leter in zip(range(len(alphabet)), alphabet):
+            self.__id_to_letter[id] = leter
+
+        self.__letter_to_id = dict()
+        for id, leter in zip(range(len(alphabet)), alphabet):
+            self.__letter_to_id[leter] = id
 
 
 def main():
