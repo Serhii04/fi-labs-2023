efb7f6ceeb41f1453da9fbe9d49ea4d47299cf18
Serhii04 <fi.03.volinec.sergij@gmail.com>
2023-04-19 18:18:02 +0300

    set_up project_class

---
+++ cp_3/volynets_fi-03_cp3/cp_proj_3.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+
+from lab_constants import __RU_ALPHABET_LETERS_PROBABILITY__ as probability
+from lab_constants import __RU_ALPHABET__ as alphabet
+
+
+class AffineCryptographer:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
